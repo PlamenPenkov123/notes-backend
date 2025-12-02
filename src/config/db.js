095876afe13567ad18280bb4612e5f1fc7ac8223
PlamenPenkov123 <plamenpenkov29@gmail.com>
2025-12-02 16:03:47 +0200

    added mongo to the docker compose

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log(`Connected to MongoDB`);
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error}`);
